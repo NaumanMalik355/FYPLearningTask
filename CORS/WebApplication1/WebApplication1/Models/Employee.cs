3699c59cecc33a1d3c1143d719b8fc57fe74117e
NaumanMalik355 <naumanm355@gmail.com>
2019-06-28 21:55:26 +0500

    Cross Origin Resource Sharing

---
+++ CORS/WebApplication1/WebApplication1/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        //[ForeignKey("ApplicationUser")]
+        //public string UserId { get; set; }
+        ////  public  ApplicationUser ApplicationUser { get; set; }
+        //public  ApplicationUser ApplicationUser { get; set; }
+
+          public string UserId { get; set; }
+          [ForeignKey("UserId")]
+        //  public  ApplicationUser ApplicationUser { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        public int EmployeeId { get; set; }
+        public string OfficeLocation { get; set; }
+
+    }
+}
